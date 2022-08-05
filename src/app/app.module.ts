5e4468595f758803bcc27de2b94d9c8307bea1f6
Edis MR <edisanthony@gmail.com>
2022-08-05 14:56:13 -0600

    initial components, services and interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/other/header/header.component';
+import { FooterComponent } from './components/other/footer/footer.component';
+import { IndexComponent } from './components/pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
