ab2eb3622532b6ac82c4dd0f248efbde28980fc6
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-08-09 16:42:32 -0300

    signing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { RegisterComponent } from './components/pages/register/register.component';
+// import { LoginComponent } from './components/pages/login/login.component';
+// import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
+// import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'forgot-password', component: ForgotPasswordComponent },
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
