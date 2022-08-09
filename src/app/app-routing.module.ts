d3f98d38af01510b04a176f825431c6041825e52
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-08-09 18:16:13 -0300

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { RegisterComponent } from './components/pages/register/register.component';
-// import { LoginComponent } from './components/pages/login/login.component';
+import { LoginComponent } from './components/pages/login/login.component';
 // import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
 // import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 
 const routes: Routes = [
   // { path: 'register', component: RegisterComponent },
-  // { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent }
   // { path: 'forgot-password', component: ForgotPasswordComponent },
   // { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/components/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['', Validators.required]
+    });
+  };
+
+  send(): any {
+    console.log(this.form.value);
+  }
+
+}
