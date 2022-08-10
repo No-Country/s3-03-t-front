5e6d99a86fc424cdec34341295b4acd7aae6e94c
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-08-10 15:37:45 -0300

    routing full & commented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+//rutas públicas
 import { IndexComponent } from './components/pages/index/index.component';
 // import { RegisterComponent } from './components/pages/register/register.component';
 import { LoginComponent } from './components/pages/login/login.component';
 // import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
+// import { AccountComponent } from './components/pages/account/account.component';
 // import { SelectRoleComponent } from './components/pages/select-role/select-role.component';
+// import { RentalComponent } from './components/pages/rental/rental.component';
+// import { ChatComponent } from './components/pages/chat/chat.component';
+// import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+
+//rutas anfitrión
+// import { CreateRentalComponent } from './components/pages/create-rental/create-rental.component';
+// import { MyRentals } from './components/pages/my-rentals/my-rentals.component';
+
+//rutas inquilino
 // import { SearchRentalComponent } from './components/pages/search-rental/search-rental.component';
 // import { SearchRoommateComponent } from './components/pages/search-roommate/search-roommate.component';
-// import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 
 const routes: Routes = [
+  //rutas públicas
   { path: '', component: IndexComponent },
   // { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
   // { path: 'forgot-password', component: ForgotPasswordComponent },
+  // { path: 'account', component: AccountComponent },
   // { path: 'select-role', component: SelectRoleComponent },
+  // { path: 'rental/:id', component: RentalComponent },
+  // { path: 'chat', component: ChatComponent },
+  // { path: '**', component: NotFoundComponent },
+
+  //rutas anfitrión
+  // { path: 'create-rental', component: CreateRentalComponent },
+  // { path: 'my-rentals', component: MyRentalsComponent },
+
+  //rutas inquilino
   // { path: 'search-rental', component: SearchRentalComponent },
   // { path: 'search-roommate', component: SearchRoommateComponent }
-  // { path: '**', component: NotFoundComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
