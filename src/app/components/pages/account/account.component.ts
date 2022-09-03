a9e0d7a0bac38dccbc5f4a71681d676c2c52a1b3
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-09-02 23:12:53 -0300

    account styling

--- src/app/components/pages/account/account.component.ts
+++ src/app/components/pages/account/account.component.ts
@@ -31,12 +31,16 @@ export class AccountComponent implements OnInit {
         this.form.get('firstName').setValue(this.user.firstName);
         this.form.get('lastName').setValue(this.user.lastName);
         this.form.get('email').setValue(this.user.email);
+        this.form.get('dni').setValue(this.user.dni);
+        this.form.get('telephone').setValue(this.user.telephone);
       });
 
     this.form = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', Validators.required],
+      dni: ['', Validators.required],
+      telephone: ['', Validators.required],
       image: ['', Validators.required]
     });
     
@@ -47,7 +51,10 @@ export class AccountComponent implements OnInit {
   }
 
   send(user: any, selectedFile: any) {
-    
+    this.userService.editUser(user, selectedFile)
+      .subscribe((res) => {
+        console.log(res);
+      })
   }
 
 }
